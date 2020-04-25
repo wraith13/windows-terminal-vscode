4fa3c28daee9cb271679ee8b825a02621e40ed8b
道化師 <wraith13@hotmail.co.jp>
2020-04-25 10:33:19 +0900

    🛠 実装 ( WIP )

--- source/extension.ts
+++ source/extension.ts
@@ -1,16 +1,21 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext)
+export const activate = (context: vscode.ExtensionContext) =>
 {
-
     console.log('Congratulations, your extension "windows-terminal" is now active!');
 
-    let disposable = vscode.commands.registerCommand('extension.helloWorld', async () => {
-        vscode.window.showInformationMessage('Hello World!');
-        await vscode.env.openExternal(vscode.Uri.parse("https://www.microsoft.com/ja-jp/p/windows-terminal-preview/9n0dx20hk701"));
-    });
-    context.subscriptions.push(disposable);
-}
+    context.subscriptions.push
+    (
+        vscode.commands.registerCommand
+        (
+            'extension.helloWorld',
+            async () =>
+            {
+                vscode.window.showInformationMessage('Hello World!');
+                await vscode.env.openExternal(vscode.Uri.parse("https://www.microsoft.com/ja-jp/p/windows-terminal-preview/9n0dx20hk701"));
+            }
+        )
+    );
+};
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export const deactivate = ( ) => { };
