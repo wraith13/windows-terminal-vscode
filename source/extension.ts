00e2b1b4b128d4317d044f053a8b1f2c69bb20e5
道化師 <wraith13@hotmail.co.jp>
2020-04-28 02:07:33 +0900

    🛠 実装 ( WIP )

--- source/extension.ts
+++ source/extension.ts
@@ -2,12 +2,51 @@ import * as vscode from 'vscode';
 import * as process from 'process';
 import * as child_process from 'child_process';
 
+interface SettingsJson
+{
+    "$schema": string;
+    defaultProfile: string;
+    copyOnSelect: boolean;
+    copyFormatting: boolean;
+    profiles: SettingsJsonProfiles;
+    schemes: unknown[];
+    keybinding: SettingsJsonKeybinding
+}
+interface SettingsJsonProfiles
+{
+    defaults: unknown[];
+    list: SettingsJsonProfileEntry[];
+}
+interface SettingsJsonProfileEntry
+{
+    guid: string;
+    name: string;
+    commandline: string;
+    hidden: boolean;
+}
+interface SettingsJsonKeybinding
+{
+    command: string | SettingsJsonKeybindingCommand;
+    keys: string;
+}
+interface SettingsJsonKeybindingCommand
+{
+    action: string;
+    singleLine: boolean;
+    split: string;
+    splitMode: string;
+}
+
 export const getStoreUri = () => vscode.Uri.parse("https://www.microsoft.com/ja-jp/p/windows-terminal-preview/9n0dx20hk701");
 export const getSettingJsonPath = () => `${process.env["USERPROFILE"]}\\AppData\\Local\\Packages\\Microsoft.WindowsTerminal_8wekyb3d8bbwe\\LocalState\\settings.json`;
 export const getCurrentFolder = () =>
     vscode.workspace.workspaceFolders && 0 < vscode.workspace.workspaceFolders.length ?
         vscode.workspace.workspaceFolders[0].uri.fsPath:
         ".";
+export const getSettingJsonDocument = async () => await vscode.workspace.openTextDocument
+(
+    getSettingJsonPath()
+);
 export const activate = (context: vscode.ExtensionContext) =>
 {
     context.subscriptions.push
@@ -24,8 +63,27 @@ export const activate = (context: vscode.ExtensionContext) =>
         ),
         vscode.commands.registerCommand
         (
+            'windowsTerminal.openProfile',
+            async () =>
+            (
+                await vscode.window.showQuickPick
+                (
+                    (<SettingsJson>JSON.parse((await getSettingJsonDocument()).getText()))
+                    .profiles.list.map
+                    (
+                        p =>
+                        ({
+                            label: p.name,
+                            command: () => child_process.exec(`wt -d ${getCurrentFolder()} -p ${p.guid}`)
+                        })
+                    )
+                )
+            )?.command()
+        ),
+        vscode.commands.registerCommand
+        (
             'windowsTerminal.openSettings',
-            async () => await vscode.window.showTextDocument(await vscode.workspace.openTextDocument(getSettingJsonPath()))
+            async () => await vscode.window.showTextDocument(await getSettingJsonDocument())
         )
     );
 };
