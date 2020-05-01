50482160fbb6fbf44a46fbd9a730a6a9c8d28306
道化師 <wraith13@hotmail.co.jp>
2020-05-01 12:28:10 +0900

    🛠 実装 ( WIP )

--- source/extension.ts
+++ source/extension.ts
@@ -1,97 +1,110 @@
-import * as vscode from 'vscode';
-import * as process from 'process';
-import * as child_process from 'child_process';
+import * as vscode from 'vscode' ;
+import * as process from 'process' ;
+import * as child_process from 'child_process' ;
+import * as Config from "./library/config" ;
+
+export const settingsJsonPath = new Config . Entry < string > ( "windowsTerminal.settingsJsonPath" ) ;
+export const defaultProfile = new Config . Entry < string > ( "windowsTerminal.defaultProfile" ) ;
+export const defaultDirectory = new Config . Entry < string > ( "windowsTerminal.defaultDirectory" ) ;
 
 interface SettingsJson
 {
-    "$schema": string;
-    defaultProfile: string;
-    copyOnSelect: boolean;
-    copyFormatting: boolean;
-    profiles: SettingsJsonProfiles;
-    schemes: unknown[];
-    keybinding: SettingsJsonKeybinding
+    "$schema" : string ;
+    defaultProfile : string ;
+    copyOnSelect : boolean ;
+    copyFormatting : boolean ;
+    profiles : SettingsJsonProfiles ;
+    schemes : unknown [ ] ;
+    keybinding : SettingsJsonKeybinding;
 }
 interface SettingsJsonProfiles
 {
-    defaults: unknown[];
-    list: SettingsJsonProfileEntry[];
+    defaults : unknown [ ] ;
+    list : SettingsJsonProfileEntry [ ] ;
 }
 interface SettingsJsonProfileEntry
 {
-    guid: string;
-    name: string;
-    commandline: string;
-    hidden: boolean;
+    guid : string ;
+    name : string ;
+    commandline : string ;
+    hidden: boolean ;
 }
 interface SettingsJsonKeybinding
 {
-    command: string | SettingsJsonKeybindingCommand;
-    keys: string;
+    command : string | SettingsJsonKeybindingCommand ;
+    keys : string ;
 }
 interface SettingsJsonKeybindingCommand
 {
-    action: string;
-    singleLine: boolean;
-    split: string;
-    splitMode: string;
+    action : string ;
+    singleLine : boolean ;
+    split : string ;
+    splitMode : string ;
 }
 
-export const getStoreUri = () => vscode.Uri.parse("https://www.microsoft.com/ja-jp/p/windows-terminal-preview/9n0dx20hk701");
-export const getSettingJsonPath = () => `${process.env["USERPROFILE"]}\\AppData\\Local\\Packages\\Microsoft.WindowsTerminal_8wekyb3d8bbwe\\LocalState\\settings.json`;
-export const getCurrentFolder = () =>
-    vscode.workspace.workspaceFolders && 0 < vscode.workspace.workspaceFolders.length ?
-        vscode.workspace.workspaceFolders[0].uri.fsPath:
-        ".";
-export const getSettingJsonDocument = async () => await vscode.workspace.openTextDocument
+export const getStoreUri = ( ) => vscode.Uri.parse ( "https://www.microsoft.com/ja-jp/p/windows-terminal-preview/9n0dx20hk701" ) ;
+export const getSettingJsonPath = async ( ) =>
+{
+    const config = settingsJsonPath . get ( "" ) ;
+    if ( null !== config && "" !== config )
+    {
+        return config;
+    }
+    return `${ process . env [ "USERPROFILE" ] }\\AppData\\Local\\Packages\\Microsoft.WindowsTerminal_8wekyb3d8bbwe\\LocalState\\settings.json` ;
+} ;
+export const getCurrentFolder = ( ) =>
+    vscode . workspace . workspaceFolders && 0 < vscode . workspace . workspaceFolders . length ?
+        vscode . workspace . workspaceFolders [ 0 ] . uri . fsPath :
+        "." ;
+export const getSettingJsonDocument = async ( ) => await vscode . workspace . openTextDocument
 (
-    getSettingJsonPath()
-);
-export const activate = (context: vscode.ExtensionContext) => context.subscriptions.push
+    await getSettingJsonPath ( )
+) ;
+export const activate = ( context : vscode . ExtensionContext ) => context . subscriptions . push
 (
-    vscode.commands.registerCommand
+    vscode . commands . registerCommand
     (
-        'windowsTerminal.showStore',
-        async () => await vscode.env.openExternal(getStoreUri())
-    ),
-    vscode.commands.registerCommand
+        'windowsTerminal.showStore' ,
+        async ( ) => await vscode . env . openExternal ( getStoreUri ( ) )
+    ) ,
+    vscode . commands . registerCommand
     (
         'windowsTerminal.open',
-        () => child_process.exec(`wt -d ${getCurrentFolder()}`)
-    ),
-    vscode.commands.registerCommand
+        ( ) => child_process . exec ( `wt -d ${ getCurrentFolder ( ) }` )
+    ) ,
+    vscode . commands . registerCommand
     (
-        'windowsTerminal.openProfile',
-        async () =>
+        'windowsTerminal.openProfile' ,
+        async ( ) =>
         (
-            await vscode.window.showQuickPick
+            await vscode . window . showQuickPick
             (
                 (
-                    <SettingsJson>JSON.parse
+                    < SettingsJson > JSON . parse
                     (
                         (
-                            await getSettingJsonDocument()
+                            await getSettingJsonDocument ( )
                         )
-                        .getText()
-                        .replace(/^\s*(\/\/.*)$/gm, "")
+                        . getText ( )
+                        . replace ( /^\s*(\/\/.*)$/gm , "" )
                     )
                 )
-                .profiles.list.map
+                . profiles . list . map
                 (
                     p =>
                     ({
-                        label: p.name,
-                        command: () => child_process.exec(`wt -d ${getCurrentFolder()} -p ${p.guid}`)
+                        label : p . name ,
+                        command : ( ) => child_process . exec ( `wt -d ${ getCurrentFolder ( ) } -p ${ p . guid }` )
                     })
                 )
             )
-        )?.command()
-    ),
-    vscode.commands.registerCommand
+        ) ?. command ( )
+    ) ,
+    vscode . commands . registerCommand
     (
-        'windowsTerminal.openSettings',
-        async () => await vscode.window.showTextDocument(await getSettingJsonDocument())
+        'windowsTerminal.openSettings' ,
+        async ( ) => await vscode . window . showTextDocument ( await getSettingJsonDocument ( ) )
     )
-);
+) ;
 
-export const deactivate = ( ) => { };
+export const deactivate = ( ) => { } ;
