54558ee74eb022bbcb06023e030a942f4f43e6f8
道化師 <645907+wraith13@users.noreply.github.com>
2020-11-22 18:01:56 +0900

    ♻️ コーディングスタイルの変更

--- source/extension.ts
+++ source/extension.ts
@@ -1,272 +1,272 @@
-import * as vscode from 'vscode' ;
-import * as process from 'process' ;
-import * as child_process from 'child_process' ;
-import * as Config from "./library/config" ;
-import * as Locale from "./library/locale" ;
-const statusBarAlignmentObject = Object . freeze
+import * as vscode from 'vscode';
+import * as process from 'process';
+import * as child_process from 'child_process';
+import * as Config from "./library/config";
+import * as Locale from "./library/locale";
+const statusBarAlignmentObject = Object.freeze
 ({
-    "none" : undefined ,
-    "left" : vscode . StatusBarAlignment . Left ,
-    "right" : vscode . StatusBarAlignment . Right ,
-}) ;
-export const statusBarText = new Config . Entry < string > ( "windowsTerminal.statusBarText" ) ;
-export const statusBarAlignment = new Config.MapEntry ( "windowsTerminal.statusBarAlignment" , statusBarAlignmentObject );
-const statusBarCommandObject = Object . freeze
+    "none": undefined,
+    "left": vscode.StatusBarAlignment.Left,
+    "right": vscode.StatusBarAlignment.Right,
+});
+export const statusBarText = new Config.Entry<string>("windowsTerminal.statusBarText");
+export const statusBarAlignment = new Config.MapEntry("windowsTerminal.statusBarAlignment", statusBarAlignmentObject);
+const statusBarCommandObject = Object.freeze
 ({
-    "windowsTerminal.open" : "Open Windows Terminal",
-    "windowsTerminal.openProfile" : "Open Windows Terminal with Profile",
-    "windowsTerminal.openSettings" : "Open Windows Terminal's settings.json"
-}) ;
-export const statusBarCommand = new Config.MapEntry ( "windowsTerminal.statusBarCommand" , statusBarCommandObject );
-export const settingsJsonPath = new Config . Entry < string > ( "windowsTerminal.settingsJsonPath" ) ;
-export const defaultProfile = new Config . Entry < string > ( "windowsTerminal.defaultProfile" ) ;
-const directoryOptionPriorityObject = Object . freeze
+    "windowsTerminal.open": "Open Windows Terminal",
+    "windowsTerminal.openProfile": "Open Windows Terminal with Profile",
+    "windowsTerminal.openSettings": "Open Windows Terminal's settings.json"
+});
+export const statusBarCommand = new Config.MapEntry("windowsTerminal.statusBarCommand", statusBarCommandObject);
+export const settingsJsonPath = new Config.Entry<string>("windowsTerminal.settingsJsonPath");
+export const defaultProfile = new Config.Entry<string>("windowsTerminal.defaultProfile");
+const directoryOptionPriorityObject = Object.freeze
 ({
-    "No specified" :
+    "No specified":
     async (
-        _getVscodeSettingValue : ( ) => Promise < string | null > ,
-        _getWindowsTerminalSettingValue : ( ) => Promise < string | null >
-    ) => null ,
-    "Prioritize Windows Terminal's settings" :
+        _getVscodeSettingValue: () => Promise<string | null>,
+        _getWindowsTerminalSettingValue: () => Promise<string | null>
+    ) => null,
+    "Prioritize Windows Terminal's settings":
     async (
-        getVscodeSettingValue : ( ) => Promise < string | null > ,
-        getWindowsTerminalSettingValue : ( ) => Promise < string | null >
-    ) => await getWindowsTerminalSettingValue ( ) ?? await getVscodeSettingValue ( ) ,
-    "Prioritize VS Code's settings" :
+        getVscodeSettingValue: () => Promise<string | null>,
+        getWindowsTerminalSettingValue: () => Promise<string | null>
+    ) => await getWindowsTerminalSettingValue () ?? await getVscodeSettingValue (),
+    "Prioritize VS Code's settings":
     async (
-        getVscodeSettingValue : ( ) => Promise < string | null > ,
-        getWindowsTerminalSettingValue : ( ) => Promise < string | null >
-    ) => await getVscodeSettingValue ( ) ?? await getWindowsTerminalSettingValue ( ) ,
-}) ;
-export const directoryOptionPriority = new Config.MapEntry ( "windowsTerminal.directoryOptionPriority" , directoryOptionPriorityObject ) ;
-export const defaultDirectory = new Config . Entry < string > ( "windowsTerminal.defaultDirectory" ) ;
-export const defaultOptions = new Config . Entry < string > ( "windowsTerminal.defaultOptions" ) ;
+        getVscodeSettingValue: () => Promise<string | null>,
+        getWindowsTerminalSettingValue: () => Promise<string | null>
+    ) => await getVscodeSettingValue () ?? await getWindowsTerminalSettingValue (),
+});
+export const directoryOptionPriority = new Config.MapEntry("windowsTerminal.directoryOptionPriority", directoryOptionPriorityObject);
+export const defaultDirectory = new Config.Entry<string>("windowsTerminal.defaultDirectory");
+export const defaultOptions = new Config.Entry<string>("windowsTerminal.defaultOptions");
 interface SettingsJson
 {
-    "$schema" : string ;
-    defaultProfile : string ;
-    copyOnSelect : boolean ;
-    copyFormatting : boolean ;
-    profiles : SettingsJsonProfiles ;
-    schemes : unknown [ ] ;
-    keybinding : SettingsJsonKeybinding;
+    "$schema": string;
+    defaultProfile: string;
+    copyOnSelect: boolean;
+    copyFormatting: boolean;
+    profiles: SettingsJsonProfiles;
+    schemes: unknown[];
+    keybinding: SettingsJsonKeybinding;
 }
 interface SettingsJsonProfiles
 {
-    defaults : unknown [ ] ;
-    list : SettingsJsonProfileEntry [ ] ;
+    defaults: unknown[];
+    list: SettingsJsonProfileEntry[];
 }
 interface SettingsJsonProfileEntry
 {
-    guid : string ;
-    name : string ;
-    commandline : string ;
-    hidden : boolean ;
-    startingDirectory ? : string ;
+    guid: string;
+    name: string;
+    commandline: string;
+    hidden: boolean;
+    startingDirectory?: string;
 }
 interface SettingsJsonKeybinding
 {
-    command : string | SettingsJsonKeybindingCommand ;
-    keys : string ;
+    command: string | SettingsJsonKeybindingCommand;
+    keys: string;
 }
 interface SettingsJsonKeybindingCommand
 {
-    action : string ;
-    singleLine : boolean ;
-    split : string ;
-    splitMode : string ;
+    action: string;
+    singleLine: boolean;
+    split: string;
+    splitMode: string;
 }
 module StatusBarItem
 {
     const create =
     (
-        properties :
+        properties:
         {
-            alignment ? : vscode . StatusBarAlignment ,
-            text ? : string ,
-            command ? : string ,
-            tooltip ? : string
+            alignment?: vscode.StatusBarAlignment,
+            text?: string,
+            command?: string,
+            tooltip?: string
         }
     )
-    : vscode . StatusBarItem =>
+    : vscode.StatusBarItem =>
     {
-        const result = vscode . window . createStatusBarItem ( properties . alignment );
-        if ( undefined !== properties . text )
+        const result = vscode.window.createStatusBarItem (properties.alignment);
+        if (undefined !== properties.text)
         {
-            result . text = properties . text;
+            result.text = properties.text;
         }
-        if ( undefined !== properties . command )
+        if (undefined !== properties.command)
         {
-            result . command = properties . command ;
+            result.command = properties.command;
         }
-        if ( undefined !== properties . tooltip )
+        if (undefined !== properties.tooltip)
         {
-            result . tooltip = properties . tooltip ;
+            result.tooltip = properties.tooltip;
         }
-        return result ;
+        return result;
     };
     let statusBarItem: vscode.StatusBarItem;
-    export const make = ( ) => statusBarItem = create
+    export const make = () => statusBarItem = create
     ({
-        alignment : statusBarAlignment . get ( "" ) ,
-        text : statusBarText . get ( "" ) ,
-        command : statusBarCommand . getKey ( "" ) ,
-        tooltip : statusBarCommand . get ( "" ) ,
+        alignment: statusBarAlignment.get (""),
+        text: statusBarText.get (""),
+        command: statusBarCommand.getKey (""),
+        tooltip: statusBarCommand.get (""),
     });
-    export const update = ( ) : void =>
+    export const update = (): void =>
     {
-        statusBarItem . text = statusBarText . get ( "" ) ;
-        statusBarItem . command = statusBarCommand . getKey ( "" ) ;
-        statusBarItem . tooltip = statusBarCommand . get ( "" ) ;
-        statusBarItem . show ( );
+        statusBarItem.text = statusBarText.get ("");
+        statusBarItem.command = statusBarCommand.getKey ("");
+        statusBarItem.tooltip = statusBarCommand.get ("");
+        statusBarItem.show();
     };
 }
-export const getStoreUri = ( ) => vscode.Uri.parse ( "https://www.microsoft.com/ja-jp/p/windows-terminal-preview/9n0dx20hk701" ) ;
-export const getDocumentUri = ( ) => vscode.Uri.parse ( "https://github.com/microsoft/terminal/tree/master/doc/user-docs" ) ;
-export const getSettingsJsonPath = async ( ) =>
+export const getStoreUri = () => vscode.Uri.parse ("https://www.microsoft.com/ja-jp/p/windows-terminal-preview/9n0dx20hk701");
+export const getDocumentUri = () => vscode.Uri.parse ("https://github.com/microsoft/terminal/tree/master/doc/user-docs");
+export const getSettingsJsonPath = async () =>
 {
-    const config = settingsJsonPath . get ( "" ) ;
-    if ( null !== config && "" !== config )
+    const config = settingsJsonPath.get("");
+    if (null !== config && "" !== config)
     {
         return config;
     }
     // settings.json のパスは決め打ちで良いっぽい。 https://github.com/microsoft/terminal/blob/master/doc/user-docs/UsingJsonSettings.md
-    return `${ process . env [ "LOCALAPPDATA" ] }\\Packages\\Microsoft.WindowsTerminal_8wekyb3d8bbwe\\LocalState\\settings.json` ;
-} ;
-export const getCurrentFolder = ( ) =>
-    vscode . workspace . workspaceFolders &&
-    0 < vscode . workspace . workspaceFolders . length ?
-        vscode . workspace . workspaceFolders [ 0 ] . uri . fsPath :
-        null ;
-export const parseJsonWithComment = ( json : string ) => JSON . parse
+    return `${ process.env [ "LOCALAPPDATA" ] }\\Packages\\Microsoft.WindowsTerminal_8wekyb3d8bbwe\\LocalState\\settings.json`;
+};
+export const getCurrentFolder = () =>
+    vscode.workspace.workspaceFolders &&
+    0 < vscode.workspace.workspaceFolders.length ?
+        vscode.workspace.workspaceFolders[0].uri.fsPath:
+        null;
+export const parseJsonWithComment = (json: string) => JSON.parse
 (
-    json . replace ( /^\s*(\/\/.*)$/gm , "" )
+    json.replace (/^\s*(\/\/.*)$/gm, "")
 );
-export const getSettingsJsonDocument = async ( ) => await vscode . workspace . openTextDocument
+export const getSettingsJsonDocument = async () => await vscode.workspace.openTextDocument
 (
-    await getSettingsJsonPath ( )
-) ;
-export const getSettings = async ( ) => < SettingsJson > parseJsonWithComment
+    await getSettingsJsonPath()
+);
+export const getSettings = async () => < SettingsJson > parseJsonWithComment
 (
     (
-        await getSettingsJsonDocument ( )
+        await getSettingsJsonDocument()
     )
-    . getText ( )
+    .getText()
 );
-export const getProfileStartingDirectory = async ( profile : string | null ) =>
+export const getProfileStartingDirectory = async (profile: string | null) =>
 {
     const settings = await getSettings();
-    profile = profile ?? settings . defaultProfile ;
-    return settings . profiles . list
-        . filter ( i => i . guid === profile ) [ 0 ] ?. startingDirectory ?? null ;
+    profile = profile ?? settings.defaultProfile;
+    return settings.profiles.list
+        .filter (i => i.guid === profile) [ 0 ] ?. startingDirectory ?? null;
 };
-export const makeProfileParam = ( profile : string | null ) => profile ? ` -p ${ profile }` : "" ;
-export const makeDirectoryParam = ( directory : string | null ) => directory ? ` -d ${ directory }` : "" ;
+export const makeProfileParam = (profile: string | null) => profile ? ` -p ${ profile }`: "";
+export const makeDirectoryParam = (directory: string | null) => directory ? ` -d ${ directory }`: "";
 export const executeWindowsTerminal =
 async (
-    data :
+    data:
     {
-        directory ? : string ,
-        profile ? : string ,
+        directory?: string,
+        profile?: string,
     }
     = { }
-) => child_process . exec
+) => child_process.exec
 (
     [
-        "wt" ,
-        defaultOptions . get ( "" ) ?? "",
-        makeProfileParam ( data . profile ?? defaultProfile . get ( "" ) ) ,
+        "wt",
+        defaultOptions.get("") ?? "",
+        makeProfileParam(data.profile ?? defaultProfile.get("")),
         makeDirectoryParam
         (
-            data . directory ??
-            await directoryOptionPriority . get ( "" )
+            data.directory ??
+            await directoryOptionPriority.get("")
             (
-                async ( ) => ( defaultDirectory . get ( "" ) ?? getCurrentFolder ( ) ),
-                async ( ) => await getProfileStartingDirectory ( data . profile ?? defaultProfile . get ( "" ) )
+                async () =>(defaultDirectory.get("") ?? getCurrentFolder()),
+                async () => await getProfileStartingDirectory(data.profile ?? defaultProfile.get(""))
             )
-        ) ,
+        ),
     ]
     .join("")
 );
-export const activate = ( context : vscode . ExtensionContext ) => context . subscriptions . push
+export const activate = (context: vscode.ExtensionContext) => context.subscriptions.push
 (
-    vscode . commands . registerCommand
+    vscode.commands.registerCommand
     (
-        'windowsTerminal.showStore' ,
-        async ( ) => await vscode . env . openExternal ( getStoreUri ( ) )
-    ) ,
-    vscode . commands . registerCommand
+        'windowsTerminal.showStore',
+        async () => await vscode.env.openExternal(getStoreUri())
+    ),
+    vscode.commands.registerCommand
     (
-        'windowsTerminal.showDocument' ,
-        async ( ) => await vscode . env . openExternal ( getDocumentUri ( ) )
-    ) ,
-    vscode . commands . registerCommand
+        'windowsTerminal.showDocument',
+        async () => await vscode.env.openExternal(getDocumentUri())
+    ),
+    vscode.commands.registerCommand
     (
-        'windowsTerminal.open' ,
-        async ( ) => await executeWindowsTerminal ( )
-    ) ,
-    vscode . commands . registerCommand
+        'windowsTerminal.open',
+        async () => await executeWindowsTerminal()
+    ),
+    vscode.commands.registerCommand
     (
-        'windowsTerminal.openProfile' ,
-        async ( ) =>
+        'windowsTerminal.openProfile',
+        async () =>
         {
             const settings = await getSettings();
             (
-                await vscode . window . showQuickPick
+                await vscode.window.showQuickPick
                 (
-                    settings . profiles . list
-                    . filter ( p => ! p . hidden )
-                    . map
+                    settings.profiles.list
+                    .filter(p => ! p.hidden)
+                    .map
                     (
                         p =>
                         ({
-                            label : p . name ,
-                            description : settings . defaultProfile === p . guid ? "default" :undefined ,
-                            detail : p . guid ,
-                            command : async ( ) => await executeWindowsTerminal ({ profile : p . guid }) ,
+                            label: p.name,
+                            description: settings.defaultProfile === p.guid ? "default": undefined,
+                            detail: p.guid,
+                            command: async () => await executeWindowsTerminal({ profile: p.guid }),
                         })
                     ),
                     {
-                        placeHolder : Locale . map ( "selectProfile" ) ,
-                        matchOnDescription : true ,
-                        matchOnDetail : true ,
+                        placeHolder: Locale.map("selectProfile"),
+                        matchOnDescription: true,
+                        matchOnDetail: true,
                     }
                 )
-            ) ?. command ( ) ;
+            )?.command();
         }
-    ) ,
-    vscode . commands . registerCommand
-    (
-        'windowsTerminal.openSettings' ,
-        async ( ) => await vscode . window . showTextDocument ( await getSettingsJsonDocument ( ) )
     ),
-    vscode . workspace . onDidChangeConfiguration
+    vscode.commands.registerCommand
+    (
+        'windowsTerminal.openSettings',
+        async () => await vscode.window.showTextDocument(await getSettingsJsonDocument())
+    ),
+    vscode.workspace.onDidChangeConfiguration
     (
         event =>
         {
             [
-                settingsJsonPath ,
-                defaultProfile ,
-                directoryOptionPriority ,
-                defaultDirectory ,
-                defaultOptions ,
-                statusBarAlignment ,
+                settingsJsonPath,
+                defaultProfile,
+                directoryOptionPriority,
+                defaultDirectory,
+                defaultOptions,
+                statusBarAlignment,
             ]
-            . map ( i => i . onDidChangeConfiguration ( event . affectsConfiguration ) ) ;
+            .map(i => i.onDidChangeConfiguration(event.affectsConfiguration));
             if
             (
                 [
-                    statusBarText ,
-                    statusBarCommand ,
+                    statusBarText,
+                    statusBarCommand,
                 ]
-                . map ( i => i . onDidChangeConfiguration ( event . affectsConfiguration ) )
-                . reduce ( ( a , b ) => a || b , true )
+                .map(i => i.onDidChangeConfiguration(event.affectsConfiguration))
+                .reduce((a, b) => a || b, true)
             )
             {
-                StatusBarItem . update ( ) ;
+                StatusBarItem.update();
             }
         }
     ),
-    StatusBarItem . make ( )
-) ;
-export const deactivate = ( ) => { } ;
+    StatusBarItem.make()
+);
+export const deactivate = () => { };
